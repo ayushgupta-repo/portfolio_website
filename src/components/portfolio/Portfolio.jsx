7a357cb9d0a97344820d376c574123902267bbb2
ayushgupta-repo <ayushgupta.8848@gmail.com>
2022-06-22 02:21:35 +0530

    For medium devices header updated, portfolio section completed and images in assets updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.webp'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.webp'
+import IMG4 from '../../assets/portfolio4.webp'
+import IMG5 from '../../assets/portfolio5.jpeg'
+import IMG6 from '../../assets/portfolio6.webp'
+
+// DO NOT USE THE IMAGES IN PRODUCTION
+const data = [
+  {
+    // Updation of demo link remained in all case
+    id: 1,
+    image: IMG1,
+    title: 'Gesturify',
+    github: 'https://github.com/ayushgupta-repo/Gesturify',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Text Detection',
+    github: 'https://github.com/ayushgupta-repo/text_detection',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Craigslist Clone',
+    github: 'https://github.com/ayushgupta-repo/craigslist',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Jarvis Voice Assistant',
+    github: 'https://github.com/ayushgupta-repo/jarvis_project',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Know The Object',
+    github: 'https://github.com/ayushgupta-repo/Know_The_Object',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Covid-19 Tracker',
+    github: 'https://github.com/ayushgupta-repo/covid-19_project',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn" target='_blank'>Github</a>
+                  <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
