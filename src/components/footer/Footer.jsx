453d6a1fa9a1102522af945a502d8f18895d03c8
ayushgupta-repo <ayushgupta.8848@gmail.com>
2022-06-25 11:46:44 +0530

    Updated socialmedia links of the foooter section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,8 +20,8 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF/></a>
-        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://www.facebook.com/profile.php?id=100007090024633"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/denverayush/"><FiInstagram/></a>
         <a href="https://twitter.com"><IoLogoTwitter/></a>
       </div>
 
